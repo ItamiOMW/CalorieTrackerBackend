b0095bc6bda207c3e9cfd353e208190656aac8e2
Вадим <itamiomw@gmail.com>
2023-10-22 09:53:51 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "com.itami.ktor-calorie-tracker"
---
+++ src/main/kotlin/com/itami/Application.kt
@@ -0,0 +1,16 @@
+package com.itami
+
+import com.itami.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureDatabases()
+    configureMonitoring()
+    configureSecurity()
+    configureRouting()
+}
